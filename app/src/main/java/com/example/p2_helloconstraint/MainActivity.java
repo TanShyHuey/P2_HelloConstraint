c7a2aa9c3e82f816dc1851756990ed8215fe66db
shy huey <shyhuey9292@gmail.com>
2020-07-01 17:27:01 +0800

    commit 4

--- app/src/main/java/com/example/p2_helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/p2_helloconstraint/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.p2_helloconstraint;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button mZero;
+    private Button mEvenOdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mZero = (Button) findViewById(R.id.button_zero);
+        mEvenOdd = (Button) findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -23,9 +30,27 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    @SuppressLint("SetTextI18n")
     public void countUp(View view) {
-        ++mCount;
-        if (mShowCount != null)
+        if(mCount == 0) {
+            mZero.setBackgroundColor(Color.MAGENTA);
+        }
+        mCount++;
+        if(mCount%2 == 0) {
+            mEvenOdd.setBackgroundColor(Color.BLUE);
+        }
+        if(mCount%2 == 1) {
+            mEvenOdd.setBackgroundColor(Color.GREEN);
+        }
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void reset(View view) {
+        mCount = 0;
+        mZero.setBackgroundColor(Color.LTGRAY);
+        mEvenOdd.setBackgroundColor(Color.BLUE);
+        if(mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
 }
